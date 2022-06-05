30c8a9fb00ea264e2cfeb6ea4551e509a770079c
Abhinav Priyadarshi <abhinavpriyadarshi2020@gmail.com>
2022-06-05 17:29:10 +0100

    new2

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# For using static file such as images in app
+<https://kanchanardj.medium.com/how-to-add-images-to-html-in-a-flask-app-4dbcc92e3aeb>
+
+# Satrting Flask App
+export FLASK_APP=app.py
+
+
+
+# Using Text recognition  using Open CV
+<https://pyimagesearch.com/2018/09/17/opencv-ocr-and-text-recognition-with-tesseract/>
---
+++ app.py
@@ -0,0 +1,22 @@
+from platform import machine
+from flask import Flask, render_template, send_file
+import requests
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html")
+
+@app.route('/download')
+def download_file():
+    path="static/Abhinav_Priyadarshi_CV.pdf"
+    return send_file(path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
